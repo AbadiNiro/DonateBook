1b658c2767c19d2fce05b18cfea833dd399e8fe8
admin <admin@admins-iMac.local>
2021-07-11 15:14:52 +0300

    add firebase to project

---
+++ DonateBook/Model/ModelFirebase.swift
@@ -0,0 +1,18 @@
+//
+//  ModelFirebase.swift
+//  DonateBook
+//
+//  Created by admin on 11/07/2021.
+//  Copyright © 2021 donatebook. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class ModelFirebase{
+    init(){
+        FirebaseApp.configure()
+    
+    }
+    
+}
