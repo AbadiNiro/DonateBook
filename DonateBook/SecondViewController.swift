040d66b6fabd129f70dc18516d3b5788482102e2
Nir Abadi <abadiniro@gmail.com>
2021-07-18 20:12:02 +0300

    add list for search

--- DonateBook/SecondViewController.swift
+++ DonateBook/SecondViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController,UITableViewDataSource,UITableViewDelegate  {
+    
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    var data = [Item]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+        
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 158
+        }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = searchTableView.dequeueReusableCell(withIdentifier: "searchListRow", for:indexPath) as! ItemTableViewCell
+        
+        let item = data[indexPath.row]
+        cell.itemName.text = item.itemName
+        cell.itemCategory.text = item.itemCategory
+        cell.itemLocation.text = item.itemLocation
+        cell.itemDescription.text = item.itemDescription
+        cell.itemContact.text = item.itemContact
+        //cell.itemImg.image = item.imgUrl
+        
+        return cell
+        }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+        }
+
+        
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Model.instance.getAllItems { items in
+            self.data = items
+            self.searchTableView.reloadData()
+            }
+            
+        }
+    
+    
+    
+    
+    }
 
 
-}
+
 
